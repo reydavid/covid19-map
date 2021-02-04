6f3742bd25f36f681fdcb6d7510eb5b8ac191579
reydavid <5302552+reydavid@users.noreply.github.com>
2021-02-03 21:55:49 -0800

    card styling updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,9 @@
 const Card = () => {
     return ( 
-        <div className="card" style={{width: "18rem"}}>
-            <div className="card-body">
-                <h5 className="card-title">.</h5>
+        <div className="card" style={{width: "15rem", marginLeft:"50px"}}>
                 <h5 className="card-title">World</h5>
                 <h5 className="card-title">Covid 19 Map</h5>
                 <h7 className="card-title">COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University</h7>
-            </div>
         </div>
     );
 }
