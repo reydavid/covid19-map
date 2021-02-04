3cf2073cdcc7788ac44d1e3ce80c0a598ee3bac3
reydavid <5302552+reydavid@users.noreply.github.com>
2021-02-03 21:51:25 -0800

    card image removed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 const Card = () => {
     return ( 
         <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."></img>
             <div className="card-body">
                 <h5 className="card-title">.</h5>
                 <h5 className="card-title">World</h5>
